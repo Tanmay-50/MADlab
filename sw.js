2c17f6836322125e5a5bfca12ed5e95bc2588e6a
Tanmay-50 <it23.tanmay.shetty@kccemsr.edu.in>
2026-03-20 11:37:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,31 @@
-const CACHE_NAME = 'hello-pwa-v1';
+const CACHE_NAME = 'hello-pwa-v2';
 const ASSETS = [
   '/',
   '/index.html',
-  'https://cdn.tailwindcss.com'
+  '/manifest.json',
+  '/icon.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => caches.match('/index.html'));
     })
   );
 });
